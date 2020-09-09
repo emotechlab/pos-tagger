12a61f65b240a907e62cbede6659d09964b2158c
xd009642 <danielmckenna93@gmail.com>
2020-09-09 23:44:50 +0100

    Add Ontov5 tags

--- src/tags.rs
+++ src/tags.rs
@@ -1,10 +1,10 @@
-
+//! TODO
 
 /// Universal Part-of-speech tags following the [Universal Dependencies
-/// scheme](https://universaldependencies.org/u/pos). 
+/// scheme](https://universaldependencies.org/u/pos).
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum Universal {
+pub enum UniversalDependencies {
     /// Examples: big, old, green, incomprehensible, first
     Adjective,
     /// Examples: in, to, during
@@ -42,5 +42,120 @@ pub enum Universal {
     /// Examples: sdijfiodufildug
     Other,
     /// Examples: ' '
-    Space
+    Space,
+}
+
+/// Part-of-speech tag set respresenting the [OntoNotes 5](catalog.ldc.upenn.edu/LDC2013T19) tags.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum OntoNotes5 {
+    Currency,
+    OpeningQuotation,
+    ClosingQuotation,
+    Comma,
+    LeftRoundBracket,
+    RightRoundBracket,
+    SentenceCloser,
+    ColonOrEllipsis,
+    Email,
+    Affix,
+    CoordinatingConjunction,
+    CardinalNumber,
+    Determiner,
+    ExistentialThere,
+    ForeignWord,
+    AdditionalWord,
+    Hyphen,
+    /// Conjunction subordinating or preposition
+    Conjunction,
+    Adjective,
+    Comparative,
+    Superlative,
+    ListItemMarker,
+    ModalAuxiliary,
+    SuperfluousPunctuation,
+    MissingTag,
+    NounSingularOrPlural,
+    NounProperSingular,
+    NounProperPlural,
+    NounPlural,
+    Predeterminer,
+    PossessiveEnding,
+    PronounPersonal,
+    PronounPossessive,
+    Adverb,
+    AdverbComparative,
+    AdverbSuperlative,
+    AdverbParticle,
+    Space,
+    Symbol,
+    InfinitivalTo,
+    Interjection,
+    VerbBaseForm,
+    VerbPastTense,
+    VerbGerundOrPresentParticiple,
+    VerbNonThirdPersonSingularPresent,
+    WhDeterminer,
+    WhPronounPersonal,
+    WhPronounPossessive,
+    WhAdverb,
+    Unknown,
+}
+
+impl From<OntoNotes5> for UniversalDependencies {
+    fn from(onto: OntoNotes5) -> Self {
+        use OntoNotes5::*;
+        match onto {
+            Currency => Self::Symbol,
+            OpeningQuotation => Self::Punctuation,
+            ClosingQuotation => Self::Punctuation,
+            Comma => Self::Punctuation,
+            LeftRoundBracket => Self::Punctuation,
+            RightRoundBracket => Self::Punctuation,
+            SentenceCloser => Self::Punctuation,
+            ColonOrEllipsis => Self::Punctuation,
+            Email => Self::Other,
+            Affix => Self::Adjective,
+            CoordinatingConjunction => Self::CoordinatingConjunction,
+            CardinalNumber => Self::Numeral,
+            Determiner => Self::Determiner,
+            ExistentialThere => Self::Pronoun,
+            ForeignWord => Self::Other,
+            AdditionalWord => Self::Other,
+            Hyphen => Self::Punctuation,
+            Conjunction => Self::Adposition,
+            Adjective => Self::Adjective,
+            Comparative => Self::Adjective,
+            Superlative => Self::Adjective,
+            ListItemMarker => Self::Other,
+            ModalAuxiliary => Self::Verb,
+            SuperfluousPunctuation => Self::Punctuation,
+            MissingTag => Self::Other,
+            NounSingularOrPlural => Self::Noun,
+            NounProperSingular => Self::ProperNoun,
+            NounProperPlural => Self::ProperNoun,
+            NounPlural => Self::Noun,
+            Predeterminer => Self::Determiner,
+            PossessiveEnding => Self::Particle,
+            PronounPersonal => Self::Pronoun,
+            PronounPossessive => Self::Determiner,
+            Adverb => Self::Adverb,
+            AdverbComparative => Self::Adverb,
+            AdverbSuperlative => Self::Adverb,
+            AdverbParticle => Self::Adposition,
+            Space => Self::Space,
+            Symbol => Self::Symbol,
+            InfinitivalTo => Self::Particle,
+            Interjection => Self::Interjection,
+            VerbBaseForm => Self::Verb,
+            VerbPastTense => Self::Verb,
+            VerbGerundOrPresentParticiple => Self::Verb,
+            VerbNonThirdPersonSingularPresent => Self::Verb,
+            WhDeterminer => Self::Determiner,
+            WhPronounPersonal => Self::Pronoun,
+            WhPronounPossessive => Self::Determiner,
+            WhAdverb => Self::Adverb,
+            Unknown => Self::Other,
+        }
+    }
 }
